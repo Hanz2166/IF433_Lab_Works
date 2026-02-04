f26912a66fca18c13d551c0303c4f371272f296e
Hanz2166 <geraldushansewaldo@gmail.com>
2026-02-04 11:20:20 +0700

    week01: refactor to use vall and string templates

--- src/week01/GradingSystem.kt
+++ src/week01/GradingSystem.kt
@@ -1,8 +1,9 @@
 package week01
 
 fun main() {
-    var name: String = "Dante"
-    var score: Int = 99
+    //REFACTOR: Gunakan val dan hapus tipe data eksplisit
+    var name = "Dante"
+    var score = 99
 
     //Concatenation gaya lama
     println("Nama: $name, Nilia: $score")
