65da751cc0e1b1900f7dd594329247741ca80fee
Hanz2166 <geraldushansewaldo@gmail.com>
2026-02-04 18:57:14 +0700

    week01: (tugas 1) clean up unused argos and comments

--- src/oop_00000106173_GeraldusHansEwaldoWiredja/week01/CircleCalculator.kt
+++ src/oop_00000106173_GeraldusHansEwaldoWiredja/week01/CircleCalculator.kt
@@ -1,24 +1,15 @@
 package oop_00000106173_GeraldusHansEwaldoWiredja.week01
 
-fun main(args: Array<String>) {
-    //variable Definition
+fun main() {
     val radius= 7.0
     val pi= 3.14
 
-    //Calculation
     var area: Double = radius * pi * radius
 
-    //Output Concatenation
     println("Radius: $radius, Area: $area")
-
-    //Logic Check
     println(CheckSize(area))
 }
 
-fun CheckSize(area: Double) {
-    if (area > 100) {
-        "It's quite big, Impressive"
-    }else {
-        "SO TINY!!"
-    }
+fun CheckSize(area: Double): String =
+    if (area > 100) "It's quite big, Impressive" else "SO TINY!!"
 }
