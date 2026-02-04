3d9efce1191c781c456ec95ed5764afd8b770921
Hanz2166 <geraldushansewaldo@gmail.com>
2026-02-04 23:35:17 +0700

    week01: (tuga s2) implementasi null saftey for user note

--- src/oop_00000106173_GeraldusHansEwaldoWiredja/week01/GameStore.kt
+++ src/oop_00000106173_GeraldusHansEwaldoWiredja/week01/GameStore.kt
@@ -3,12 +3,13 @@ package oop_00000106173_GeraldusHansEwaldoWiredja.week01
 fun main() {
     val Halo = "Halo" to 250000
     val RDR2 = "RDR2" to 550000
+    val userNote: String? = null
 
     val HaloDiscountPrice = calculateDiscount(Halo.second)
     val RDR2DiscountPrice = calculateDiscount(RDR2.second)
 
-    printReceipt(title = Halo.first, finalPrice = HaloDiscountPrice)
-    printReceipt(title = RDR2.first, finalPrice = RDR2DiscountPrice)
+    printReceipt(title = Halo.first, finalPrice = HaloDiscountPrice, note = userNote ?: "No Notes")
+    printReceipt(title = RDR2.first, finalPrice = RDR2DiscountPrice, note = userNote ?: "No Notes")
 }
 
 fun calculateDiscount(price: Int): Int {
@@ -19,7 +20,8 @@ fun calculateDiscount(price: Int): Int {
     }
 }
 
-fun printReceipt(title: String, finalPrice: Int) {
-    println("Game: $title, Discount Price: $finalPrice")
+fun printReceipt(title: String, finalPrice: Int, note: String) {
+    println("Game: $title, Discount Price: Rp $finalPrice")
+    println("Customer Note: $note")
     println("-----------------------------------------")
 }
