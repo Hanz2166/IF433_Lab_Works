4043efb70333488189e21287f21c6e365589b469
Hanz2166 <geraldushansewaldo@gmail.com>
2026-02-04 17:56:36 +0700

    week01: (tugas 1) add legacy CircleCalculator code

---
+++ src/oop_00000106173_GeraldusHansEwaldoWiredja/week01/CircleCalculator.kt
@@ -0,0 +1,17 @@
+package oop_00000106173_GeraldusHansEwaldoWiredja.week01
+
+fun main(args: Array<String>) {
+    //variable Definition
+    var radius: Double = 7.0
+    var pi: Double = 3.14
+
+    //Calculation
+    var area: Double = radius * pi * radius
+
+    //Output Concatenation
+    println("Radius: " + radius + ", Area: " + area)
+
+    //Logic Check
+    CheckSize(area)
+
+}
