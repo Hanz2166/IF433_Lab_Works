9fe2367d4f106f09eb8c075181c8d35093dbcd7c
Hanz2166 <geraldushansewaldo@gmail.com>
2026-02-04 22:32:18 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000106173_GeraldusHansEwaldoWiredja/week01/GameStore.kt
+++ src/oop_00000106173_GeraldusHansEwaldoWiredja/week01/GameStore.kt
@@ -1,5 +1,17 @@
 package oop_00000106173_GeraldusHansEwaldoWiredja.week01
 
-fun main(){
+fun main() {
     val Halo = "Halo" to 250000
     val RDR2 = "RDR2" to 550000
+
+    val HaloDiscountPrice = calculateDiscount(Halo.second)
+    val RDR2DiscountPrice = calculateDiscount(RDR2.second)
+}
+
+fun calculateDiscount(price: Int): Int {
+    return if(price > 500000) {
+        price - (price * 10/100)
+    } else {
+        price
+    }
+}
