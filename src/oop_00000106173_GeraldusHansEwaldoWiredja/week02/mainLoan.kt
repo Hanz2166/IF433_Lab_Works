d7c4290124d3de8aa353c885afee8feaa3354325
Hanz2166 <geraldushansewaldo@gmail.com>
2026-02-15 14:53:43 +0700

    week02: (task 1) implement library input system

--- src/oop_00000106173_GeraldusHansEwaldoWiredja/week02/mainLoan.kt
+++ src/oop_00000106173_GeraldusHansEwaldoWiredja/week02/mainLoan.kt
@@ -3,7 +3,27 @@ package oop_00000106173_GeraldusHansEwaldoWiredja.week02
 import java.util.Scanner
 
 fun main() {
-    val scanner = Scanner(System.'in')\
+    val scanner = Scanner(System.`in`)
 
     println(" --- APLIKASI PEMINJAMAN BUKU PERPUSTAKAAN --- ")
     println("Nama Buku Yang Ingin Dipinjam: ")
+    val bookTitle = scanner.nextLine()
+
+    print("Nama Peminjam: ")
+    val borrower = scanner.next()
+
+    println("Pinjam untuk berapa lama: ")
+    var loanDuration = scanner.nextInt()
+
+    scanner.nextline()
+
+    if(loanDuration < 0) {
+        loanDuration = 1
+    }
+    val l1 = Loan(bookTitle, borrower, loanDuration)
+    println("Status: Pendaftaran Selesai. ")
+    println("BookTitle      : ${L1.bookTitle}")
+    println("Peminjam       : ${L1.borrower}")
+    println("Durasi         : ${L1.loanDuration} hari")
+    println("Total Denda    : ${L1.calculateFine()}")
+}
