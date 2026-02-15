9cbd18c19e77c4787d3a9d39430c178bb69879ab
Hanz2166 <geraldushansewaldo@gmail.com>
2026-02-15 11:53:15 +0700

    week02: (task 1) create Loan class with default args

---
+++ src/oop_00000106173_GeraldusHansEwaldoWiredja/week02/mainLoan.kt
@@ -0,0 +1,9 @@
+package oop_00000106173_GeraldusHansEwaldoWiredja.week02
+
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.'in')\
+
+    println(" --- APLIKASI PEMINJAMAN BUKU PERPUSTAKAAN --- ")
+    println("Nama Buku Yang Ingin Dipinjam: ")
