cf7c6f1072026a01b9ac906044b4935c506bffc3
Hanz2166 <geraldushansewaldo@gmail.com>
2026-02-18 10:56:20 +0700

    week03: cleanup code

--- src/oop_00000106173_GeraldusHansEwaldoWiredja/weeok03/Main.kt
+++ src/oop_00000106173_GeraldusHansEwaldoWiredja/weeok03/Main.kt
@@ -3,14 +3,10 @@ package oop_00000106173_GeraldusHansEwaldoWiredja.weeok03
 fun main(){
     val e = Employee("Budi")
 
-    //1. test validasi salary
-    e.salary = -1000
-    e.salary = 5000000
+
     println("Gaji: ${e.salary}")
 
-    //2. tes encapsulation
     e.increasePerformance()
 
-    //3. test computed property
     println("Pajak harus dibayr: ${e.salary}")
 }
