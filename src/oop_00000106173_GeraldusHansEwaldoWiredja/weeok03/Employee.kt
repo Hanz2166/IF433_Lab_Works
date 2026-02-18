3a47d559e3f826f62357a45ee03807b93d76b704
Hanz2166 <geraldushansewaldo@gmail.com>
2026-02-18 10:25:30 +0700

    week03: simulate stack overflow error in setter

--- src/oop_00000106173_GeraldusHansEwaldoWiredja/weeok03/Employee.kt
+++ src/oop_00000106173_GeraldusHansEwaldoWiredja/weeok03/Employee.kt
@@ -2,4 +2,8 @@ package oop_00000106173_GeraldusHansEwaldoWiredja.weeok03
 
 class Employee (val name: String){
     var salary: Int = 0
+        set(value) {
+            println("Mencoba get gaji ke: $value")
+            this.salary = value
+        }
 }
---
+++ src/oop_00000106173_GeraldusHansEwaldoWiredja/weeok03/Main.kt
@@ -0,0 +1,7 @@
+package oop_00000106173_GeraldusHansEwaldoWiredja.weeok03
+
+fun main(){
+    val e = Employee("Budi")
+    println("Test Error")
+    e.salary = 5000
+}
