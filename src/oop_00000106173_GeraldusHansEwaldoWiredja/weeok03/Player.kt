a5649f09e41c630f894b8467b890efddf622a4eb
Hanz2166 <geraldushansewaldo@gmail.com>
2026-02-18 12:53:01 +0700

    week03: (task 2) create Player class with private xp

---
+++ src/oop_00000106173_GeraldusHansEwaldoWiredja/weeok03/Player.kt
@@ -0,0 +1,23 @@
+package oop_00000106173_GeraldusHansEwaldoWiredja.weeok03
+
+class Player (val userName: String) {
+    private var EXP: Int = 0
+
+    val level: Int
+        get() = (EXP / 100) + 2
+
+    fun recieveEXP (amount: Int) {
+        if (amount <= 0) {
+            println("EXP haru positif")
+            return
+        }
+        val oldLevel = level
+
+        EXP += amount
+        val newLevel = level
+
+        if(newLevel > oldLevel) {
+            println("Level Up! Selamat ${userName} naik ke level ${newLevel}")
+        }
+    }
+}
