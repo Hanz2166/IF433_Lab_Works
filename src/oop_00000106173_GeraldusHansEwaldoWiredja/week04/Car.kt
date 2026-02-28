b35997c1625ae6858fd68dad32534d27f9936f1f
Hanz2166 <geraldushansewaldo@gmail.com>
2026-02-28 13:56:34 +0700

    week04: (task 1) create ElectricCar class with final override

--- src/oop_00000106173_GeraldusHansEwaldoWiredja/week04/Car.kt
+++ src/oop_00000106173_GeraldusHansEwaldoWiredja/week04/Car.kt
@@ -2,7 +2,7 @@ package oop_00000106173_GeraldusHansEwaldoWiredja.week04
 
 class Car(brand: String, val numberOfDoors: Int) : Vehicle(brand) {
     fun openTrunk() {
-        println("Bagasi mobil $brand dengan $numberOfDoors [intu dibuka")
+        println("Bagasi mobil $brand dengan $numberOfDoors pintu dibuka")
     }
     override fun honk() {
         println("Ayaya! Ayaya! Mobil $brand lewat!!")
