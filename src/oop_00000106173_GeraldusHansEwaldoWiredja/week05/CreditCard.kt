4ffe9cc6ec3ca4f02d0664f0edddfb0959f55f53
Hanz2166 <geraldushansewaldo@gmail.com>
2026-03-09 21:00:13 +0700

    week05: (task 2) create CreditCard class

---
+++ src/oop_00000106173_GeraldusHansEwaldoWiredja/week05/CreditCard.kt
@@ -0,0 +1,14 @@
+package oop_00000106173_GeraldusHansEwaldoWiredja.week05
+
+abstract class CreditCard (accountName: String, val limit: Double): PaymentMethod (accountName) {
+    var usedAmount = 0.0
+
+    override fun processPayment(amount: Double) {
+        if(usedAmount + amount <= limit) {
+            usedAmount += amount
+            println("Proses pembayaran berhail")
+        }else {
+            println("Traksaksi di tolak. Kartu sudah mencapai limit")
+        }
+    }
+}
