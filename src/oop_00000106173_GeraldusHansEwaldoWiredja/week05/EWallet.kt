a962ffbd7a09f7e2e770fbf0be63a25ac5eec8c8
Hanz2166 <geraldushansewaldo@gmail.com>
2026-03-09 20:32:28 +0700

    week05: (task 2) create EWallet class

---
+++ src/oop_00000106173_GeraldusHansEwaldoWiredja/week05/EWallet.kt
@@ -0,0 +1,17 @@
+package oop_00000106173_GeraldusHansEwaldoWiredja.week05
+
+abstract class EWallet (accountName: String, var balance: Double) : PaymentMethod(accountName) {
+    override fun processPayment(amount: Double) {
+        if(balance >= amount) {
+            balance -= amount
+            println("Trasaksi berhasil. Sisa saldo: $balance")
+        } else {
+            println("Transaksi gagal. Saldo tidak mencukupi.")
+        }
+    }
+
+    fun topUp(amount: Double) {
+        balance += amount
+        println("Top up saldo $amount berhasil. Balance sekarang: $balance")
+    }
+}
