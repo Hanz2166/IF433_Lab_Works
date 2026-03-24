87f74a49c74df4e01d8ea92fdceca715ce7fad69
Hanz2166 <geraldushansewaldo@gmail.com>
2026-03-24 12:49:23 +0700

    week06: (task) create SmartSpeaker

---
+++ src/week06/SmartSpeaker.kt
@@ -0,0 +1,14 @@
+package week06
+
+class SmartSpeaker (override id: String,override name: String): SmartDevice, Switchable {
+    override fun turnOn() {
+        println("$name speaker turned on")
+    }
+    override fun turnOff() {
+        println("$name speaker turned off")
+    }
+
+    fun playMusic(Song: String) {
+        println("$Song is currently playing")
+    }
+}
