656c2cacb5d39ab588e6afce3f090d8a3517e62c
Hanz2166 <geraldushansewaldo@gmail.com>
2026-03-24 13:09:28 +0700

    week06: (task) implement advanced smart casting in Hub

--- src/week06/SmartHomeHub.kt
+++ src/week06/SmartHomeHub.kt
@@ -3,15 +3,18 @@ package week06
 class SmartHome {
     val devices = mutableListOf<SmartDevice>()
 
-    fun addDevice(device: SmartDevice) {
-        devices.add(device)
-    }
+    fun addDevice(device: SmartDevice) {devices.add(device)}
 
     fun turnOffAllSwitches() {
         for (device in devices) {
-            if (device is Switchable) {
-                device.turnOff()
-            }
+            if (device is Switchable) {device.turnOff() }
+        }
+    }
+
+    fun activateSecurityMode() {
+        for (device in devices) {
+            if (device is Record){device.startRecord()}
+            if (device is SmartSpeaker){device.playMusic("Wedding Bells")}
         }
     }
 }
