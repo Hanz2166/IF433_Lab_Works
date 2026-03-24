4e34f320b5b64d2f7883c1a840100fb7f224ec43
Hanz2166 <geraldushansewaldo@gmail.com>
2026-03-24 12:40:47 +0700

    week06: (task) create smart home interfaces

---
+++ src/week06/SmartInterface.kt
@@ -0,0 +1,10 @@
+package week06
+
+interface SmartDevice {val id: String, val name: String}
+interface Switchable {fun turnOn(), fun turnOff()}
+interface Record {
+    fun startRecord()
+    fun stopRecord(){
+        println("Stop record, save to cloud")
+    }
+}
