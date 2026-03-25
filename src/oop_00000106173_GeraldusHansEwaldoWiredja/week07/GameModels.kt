6536961185e9ea2bc78818318303e0f991922052
Hanz2166 <geraldushansewaldo@gmail.com>
2026-03-25 16:49:43 +0700

    week07: (task) create ItemRarity enum

---
+++ src/oop_00000106173_GeraldusHansEwaldoWiredja/week07/GameModels.kt
@@ -0,0 +1,9 @@
+package oop_00000106173_GeraldusHansEwaldoWiredja.week07
+
+enum class ItemRarity(val dropChance: Int) {
+    COMMON(60),
+    UNCOMMON(30),
+    RARE(20),
+    EPIC(7),
+    LEGENDARY(3)
+}
