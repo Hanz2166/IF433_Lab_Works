f014dc23276ab2521d0975f056c687772e0978e2
Hanz2166 <geraldushansewaldo@gmail.com>
2026-03-25 14:30:23 +0700

    week07: (trial) demonstrate regular class equality failure

--- src/oop_00000106173_GeraldusHansEwaldoWiredja/week07/Main.kt
+++ src/oop_00000106173_GeraldusHansEwaldoWiredja/week07/Main.kt
@@ -8,4 +8,10 @@ fun main() {
     println("\n=== TEST COMPANION OBJECT ===")
     val client = NetworkClient.createClient()
     client.connect()
+
+    println("\n=== TEST REGULAR CLASS ===")
+    val reg1 = RegularUser("Roland", 33)
+    val reg2 = RegularUser("Roland", 33)
+    println(reg1) // Akan mencetak memori hash
+    println("Sama? ${reg1 == reg2}") // False
 }
