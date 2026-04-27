04fa26d957c90d3f87b69f8199a44be9a11b6047
Hanz2166 <geraldushansewaldo@gmail.com>
2026-04-27 20:30:15 +0700

    week09: (task) chain sortedBy and map for worst performers

--- src/oop_00000106173_GeraldusHansEwaldoWiredja/week09/TradingDashboard.kt
+++ src/oop_00000106173_GeraldusHansEwaldoWiredja/week09/TradingDashboard.kt
@@ -17,5 +17,7 @@ fun main() {
     val topPerformersString = winningTrades
         .sortedByDescending { it.roe }
         .map { "WIN [${it.pair} ${it.position}]: +${it.roe}% ROE (Lev: ${it.leverage}x)" }
-
+    val worstPerformersString = losingTrades
+        .sortedBy { it.roe } // Diurutkan dari yang minusnya paling besar
+        .map { "LOSS [${it.pair} ${it.position}]: ${it.roe}% ROE (Lev: ${it.leverage}x)" }
 }
