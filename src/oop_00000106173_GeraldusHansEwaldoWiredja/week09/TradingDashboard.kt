6dc4520139b7e6b37346a51362eff131b94e21bd
Hanz2166 <geraldushansewaldo@gmail.com>
2026-04-27 20:06:51 +0700

    week09: (task) populate mock trade history list

---
+++ src/oop_00000106173_GeraldusHansEwaldoWiredja/week09/TradingDashboard.kt
@@ -0,0 +1,13 @@
+package oop_00000106173_GeraldusHansEwaldoWiredja.week09
+
+fun main() {
+    // Pipeline Inisialisasi Data Uji (Syarat data dipenuhi)
+    val tradeHistory = listOf(
+        TradeLog("BTCUSDT", "LONG", 20, 15.5, "CLOSED"),
+        TradeLog("ETHUSDT", "SHORT", 10, -5.2, "CLOSED"),
+        TradeLog("BTCUSDT", "SHORT", 20, -12.0, "CLOSED"),
+        TradeLog("ETHUSDT", "LONG", 10, 25.0, "CLOSED"),
+        TradeLog("SOLUSDT", "LONG", 5, 8.5, "OPEN"),
+        TradeLog("ADAUSDT", "SHORT", 5, -2.0, "OPEN")
+    )
+}
