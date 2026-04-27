d8d1daf1e848d0c69e2cd2c3c25e485eab860ccd
Hanz2166 <geraldushansewaldo@gmail.com>
2026-04-27 20:32:10 +0700

    week09: (task) print top performers using forEach

--- src/oop_00000106173_GeraldusHansEwaldoWiredja/week09/TradingDashboard.kt
+++ src/oop_00000106173_GeraldusHansEwaldoWiredja/week09/TradingDashboard.kt
@@ -23,4 +23,6 @@ fun main() {
     val uniquePairs = tradeHistory
         .map { it.pair }
         .toSet()
+    println("=== CRYPTO TRADING DASHBOARD ===")
+    topPerformersString.forEach { println(it) }
 }
