0786ed53e59f2292f48907b67014e30eeb16b217
Hanz2166 <geraldushansewaldo@gmail.com>
2026-05-05 19:37:16 +0700

    week10: (task) populate coin repository

--- src/oop_00000106173_GeraldusHansEwaldoWiredja/week10/CryptoDashboard.kt
+++ src/oop_00000106173_GeraldusHansEwaldoWiredja/week10/CryptoDashboard.kt
@@ -1,3 +1,6 @@
 package oop_00000106173_GeraldusHansEwaldoWiredja.week10
 
 val coinRepo = WalletRepository<Coin>()
+coinRepo.add(Coin("BTC", 0.5))
+coinRepo.add(Coin("ETH", 10.2))
+coinRepo.add(Coin("USDT", 500.0))
