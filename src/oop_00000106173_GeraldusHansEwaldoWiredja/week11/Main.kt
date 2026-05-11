4ecb86e66edbf1a1323951897aa14539da990baa
Hanz2166 <geraldushansewaldo@gmail.com>
2026-05-11 19:06:11 +0700

    week11: implement with function for multiple operations

--- src/oop_00000106173_GeraldusHansEwaldoWiredja/week11/Main.kt
+++ src/oop_00000106173_GeraldusHansEwaldoWiredja/week11/Main.kt
@@ -1,20 +1,6 @@
 package oop_00000106173_GeraldusHansEwaldoWiredja.week11
 
 fun main() {
-    println("=== Test Apply ===")
-    val user = User().apply{
-        name = "Alex"
-        age = 25
-    }
-    println(user)
-
-    println("\n=== Test Also ===")
-    val numbers = mutableListOf(1, 2, 3)
-    numbers.also {
-        println("Log sebelum ditabmbah: $it")
-    }.add(4)
-    println("Setelah ditambah: $numbers")
-
     println("\n=== Test Extension Functions ===")
     println("Alen".addGreeting())
     println("Hi".repeatTimes(3))
--- src/oop_00000106173_GeraldusHansEwaldoWiredja/week11/ScopeConfiguration.kt
+++ src/oop_00000106173_GeraldusHansEwaldoWiredja/week11/ScopeConfiguration.kt
@@ -1,3 +1,24 @@
 package oop_00000106173_GeraldusHansEwaldoWiredja.week11
 
 data class User(var name: String = "", var age: Int = 0)
+
+fun main(){
+    println("=== Test Apply ===")
+    val user = User().apply{
+        name = "Alex"
+        age = 25
+    }
+    println(user)
+
+    println("\n=== Test Also ===")
+    val numbers = mutableListOf(1, 2, 3)
+    numbers.also {
+        println("Log sebelum ditabmbah: $it")
+    }.add(4)
+    println("Setelah ditambah: $numbers")
+
+    println("\n=== Test With ===")
+    with(user){
+        println("User Detail -> Nama: $name, Umur: $age")
+    }
+}
