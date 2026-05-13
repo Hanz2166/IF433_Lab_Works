f96e2c44f0d0cffb6b84d626ec01e1990c3562ad
Hanz2166 <geraldushansewaldo@gmail.com>
2026-05-13 11:01:38 +0700

    week12: implement multiple catch blocks

--- src/oop_00000106173_GeraldusHansEwaldoWiredja/week12/CustomException.kt
+++ src/oop_00000106173_GeraldusHansEwaldoWiredja/week12/CustomException.kt
@@ -17,3 +17,18 @@ class BankAccount(var balance: Double) {
         println("Withdraw Success. Remaining balance: $balance")
     }
 }
+
+fun main() {
+    println("=== Test Multiple Catch ===")
+    val account = BankAccount(100.0)
+
+    try{
+        account.withdraw(1500.0)// <- Trigger InssuficientException
+    }catch(e: InsufficientFundsException){
+        println("Caught Domain Error: Uang tidak cukup.   ${e.message}")
+    }catch(e: IllegalStateException){
+        println("Caught Domain Error: Input tidak valid.   ${e.message}")
+    }catch(e: Exception){
+        println("Caught General Error: Terjadi kesalahan tidak terduga.   ${e.message}")
+    }
+}
