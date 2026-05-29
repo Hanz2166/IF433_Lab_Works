b918036352cfdd6f68af59d4bcd8a50387c60279
Hanz2166 <geraldushansewaldo@gmail.com>
2026-05-29 13:09:29 +0700

    week12: (task) handle feeding execution with multiple catch

--- src/oop_00000106173_GeraldusHansEwaldoWiredja/week12/SmartFeeder.kt
+++ src/oop_00000106173_GeraldusHansEwaldoWiredja/week12/SmartFeeder.kt
@@ -13,4 +13,16 @@ fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): I
 
 fun main() {
     var currentKibbleStock = 50
+
+    println("=== Simulasi Jadwal Makan Pagi ===")
+    try {
+        currentKibbleStock = dispenseKibble(requestedGram = 80, availableGram = currentKibbleStock, isJammed = false)
+        println("Pagi: Kibble terdispensi berhasil. Sisa stok: $currentKibbleStock gr")
+    } catch (e: DispenserJamException) {
+        println("Error Pagi: ${e.message}")
+    } catch (e: FoodEmptyException) {
+        println("Error Pagi: ${e.message}")
+    } catch (e: Exception) {
+        println("Error Pagi (General): ${e.message}")
+    }
 }
